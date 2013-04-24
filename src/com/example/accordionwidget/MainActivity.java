94f7c08ad440391e8ddaeb03b36e3bc1d47a44fb
Mona man <mman@uci.edu>
2013-04-24 09:05:31 -0700

    Worked on some of many tickets... but the code runs.

--- src/com/example/accordionwidget/MainActivity.java
+++ src/com/example/accordionwidget/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.view.LayoutInflater;
 import android.view.ViewGroup.LayoutParams;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.PopupWindow;
 import android.content.Context;
 import android.content.Intent;
@@ -24,12 +25,16 @@ public class MainActivity extends Activity {
 	//	addListenerOnButton();
 
 		Button btnNFC = (Button) findViewById(R.id.btnNFC);
+		btnNFC.setEnabled(false);
 		Button btnVISUAL = (Button) findViewById(R.id.btnVISUAL);
+		btnVISUAL.setEnabled(false);
 		Button btnNETWORK = (Button) findViewById(R.id.btnNETWORK);
+		btnNETWORK.setEnabled(false);
 		Button btnRESULT = (Button) findViewById(R.id.btnRESULT);
+		btnRESULT.setEnabled(false);
 
 		final View panelNFC = findViewById(R.id.panelNfc);
-		panelNFC.setVisibility(View.GONE);
+		panelNFC.setVisibility(View.VISIBLE);
 
 		View panelVisual = findViewById(R.id.panelVisual);
 		panelVisual.setVisibility(View.GONE);
@@ -40,8 +45,10 @@ public class MainActivity extends Activity {
 		View panelResult = findViewById(R.id.panelResult);
 		panelResult.setVisibility(View.GONE);
 		
+		// since buttons are no longer clickable, listeners are no longer needed
+		
 		/* STEP 1*/
-		btnNFC.setOnClickListener(new OnClickListener() {
+/*		btnNFC.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// DO STUFF
@@ -59,14 +66,13 @@ public class MainActivity extends Activity {
 
 			}
 		});
-		
+*/		
 		final Button btnOpenPopup = (Button) findViewById(R.id.scan);
 		btnOpenPopup.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
-						.getSystemService(LAYOUT_INFLATER_SERVICE);
+				LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
 				View popupView = layoutInflater.inflate(R.layout.popup, null);
 				final PopupWindow popupWindow = new PopupWindow(popupView,
 						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
@@ -102,7 +108,8 @@ public class MainActivity extends Activity {
          
 		}});
 		/* STEP 2 */
-		btnVISUAL.setOnClickListener(new OnClickListener() {
+
+/*		btnVISUAL.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// DO STUFF
@@ -125,6 +132,7 @@ public class MainActivity extends Activity {
 				panelResult.setVisibility(View.GONE);
 			}
 		});	
+*/
 		final Button snapPic = (Button) findViewById(R.id.snappic);
 		snapPic.setOnClickListener(new Button.OnClickListener() {
 
@@ -138,12 +146,17 @@ public class MainActivity extends Activity {
 				panelSnappic2.setVisibility(View.VISIBLE);     
 		}});
 		
-		final Button visualScanSkip = (Button) findViewById(R.id.skip);
-		visualScanSkip.setOnClickListener(new Button.OnClickListener() {
+		final Button visualScanEnter = (Button) findViewById(R.id.enter);
+		visualScanEnter.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				// add view for button
+				InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE); 
+
+				inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                           InputMethodManager.HIDE_NOT_ALWAYS);
+				
 				View panelProfile = findViewById(R.id.panelNfc);
 				panelProfile.setVisibility(View.GONE);
 
@@ -176,8 +189,45 @@ public class MainActivity extends Activity {
 				panelResult.setVisibility(View.GONE);
 		}});
 		
+		final Button visualScanSkip = (Button) findViewById(R.id.skip);
+		visualScanSkip.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// add view for button
+				View panelProfile = findViewById(R.id.panelNfc);
+				panelProfile.setVisibility(View.GONE);
+
+				View panelSettings = findViewById(R.id.panelVisual);
+				panelSettings.setVisibility(View.GONE);
+
+				View panelPrivacy = findViewById(R.id.panelNetwork);
+				panelPrivacy.setVisibility(View.VISIBLE);  
+				
+				View panelResult = findViewById(R.id.panelResult);
+				panelResult.setVisibility(View.GONE);
+		}});
+		
 		/* STEP 3*/
-		btnNETWORK.setOnClickListener(new OnClickListener() {
+		final Button networkScanSkip = (Button) findViewById(R.id.temporaryskip);
+		networkScanSkip.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// add view for button
+				View panelProfile = findViewById(R.id.panelNfc);
+				panelProfile.setVisibility(View.GONE);
+
+				View panelSettings = findViewById(R.id.panelVisual);
+				panelSettings.setVisibility(View.GONE);
+
+				View panelPrivacy = findViewById(R.id.panelNetwork);
+				panelPrivacy.setVisibility(View.GONE);  
+				
+				View panelResult = findViewById(R.id.panelResult);
+				panelResult.setVisibility(View.VISIBLE);
+		}});
+/*		btnNETWORK.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// DO STUFF
@@ -195,12 +245,13 @@ public class MainActivity extends Activity {
 
 			}
 		});
+*/
 		final Button scanAccept = (Button) findViewById(R.id.accept);
 		scanAccept.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				// DETAILS/HISTORY POPUP
+				// DETAILS/HISTORY POPUP or adds scan details to history???
 				
 		}});
 		final Button scanReject = (Button) findViewById(R.id.reject);
@@ -221,9 +272,28 @@ public class MainActivity extends Activity {
 				View panelResult = findViewById(R.id.panelResult);
 				panelResult.setVisibility(View.GONE);
 		}});
+		final Button scanReset = (Button) findViewById(R.id.reset);
+		scanReset.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// add view for button
+				View panelProfile = findViewById(R.id.panelNfc);
+				panelProfile.setVisibility(View.VISIBLE);
+
+				View panelSettings = findViewById(R.id.panelVisual);
+				panelSettings.setVisibility(View.GONE);
+
+				View panelPrivacy = findViewById(R.id.panelNetwork);
+				panelPrivacy.setVisibility(View.GONE);  
+				
+				View panelResult = findViewById(R.id.panelResult);
+				panelResult.setVisibility(View.GONE);
+		}});
 		
 		/* STEP 4 */
-		btnRESULT.setOnClickListener(new OnClickListener() {
+		
+/*		btnRESULT.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// DO STUFF
@@ -241,7 +311,7 @@ public class MainActivity extends Activity {
 
 			}
 		});	
-	
+*/	
 		
 
 	}
